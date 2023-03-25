50a09cd952aa1c9cccb09bb297b27db6d08b58b4
Fewnity <electronic.microprosse@gmail.com>
2023-03-25 03:17:06 +0100

    Create performance profiler system

--- Xenity_Engine/Source/debug/performance.h
+++ Xenity_Engine/Source/debug/performance.h
@@ -3,6 +3,32 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <unordered_map>
+
+class ProfilerValue 
+{
+public:
+	void AddValue(int v) {
+		value += v;
+		addedValue += v;
+	}
+	void ResetValue() 
+	{
+		lastValue = value;
+		value = 0;
+	}
+	int GetValue() {
+		return lastValue;
+	}
+	void SetLastValue(int v) {
+		lastValue = v;
+	}
+	long long average = 0;
+	long long addedValue = 0;
+private:
+	long long lastValue = 0;
+	long long value = 0;
+};
 
 class Performance
 {
@@ -12,8 +38,18 @@ public:
 	static void AddMaterialUpdate();
 	static int GetDrawCallCount();
 	static int GetUpdatedMaterialCount();
+	static std::unordered_map<std::string, ProfilerValue*> profilerList;
+	static void Update();
+	static void EnableProfiler(bool enable);
+	static bool IsProfilerEnabled();
 
 private:
+	
+	static void ResetProfiler();
 	static int drawCallCount;
 	static int updatedMaterialCount;
+
+	static int tickCount;
+	static float averageCoolDown;
+	static bool profilerEnabled;
 };
