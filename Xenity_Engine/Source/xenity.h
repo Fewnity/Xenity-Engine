abcbe35aaf70f873338a9934d546bce0e0691b5d
Fewnity <electronic.microprosse@gmail.com>
2023-04-20 10:49:45 +0200

    Add a way to load all useful includes of the engine

--- Xenity_Engine/Source/xenity.h
+++ Xenity_Engine/Source/xenity.h
@@ -1,1 +1,72 @@
 #pragma once
+
+#include "monobehaviour.h"
+#include "component.h"
+
+#include "asset_manager.h"
+
+//Engine
+#include "engine.h"
+#include "engine_settings.h"
+
+//GameObjects
+#include "gameobject.h"
+#include "transform.h"
+
+//Vectors
+#include "vectors/vector2.h"
+#include "vectors/vector3.h"
+#include "vectors/vector4.h"
+
+//UI
+#include "ui/window.h"
+#include "ui/ui_manager.h"
+#include "ui/TextAlignments.h"
+
+//Tools
+#include "tools/benchmark.h"
+#include "tools/curve.h"
+#include "tools/math.h"
+#include "tools/profiler_benchmark.h"
+#include "tools/shape_spawner.h"
+
+//Time
+#include "time/time.h"
+
+//Scenes
+//#include "scene_manager/scene.h"
+//#include "scene_manager/SceneManager.h"
+
+//Lighting
+#include "lighting/lighting.h"
+
+//inputs
+#include "inputs/input_system.h"
+
+//Graphics
+#include "graphics/camera.h"
+#include "graphics/camera_projection_types.h"
+#include "graphics/graphics.h"
+#include "graphics/iDrawable.h"
+#include "graphics/iDrawableTypes.h"
+#include "graphics/material.h"
+#include "graphics/mesh.h"
+#include "graphics/shader.h"
+#include "graphics/sprite_manager.h"
+#include "graphics/spriteRenderer.h"
+#include "graphics/texture.h"
+#include "graphics/text_renderer.h"
+#include "graphics/tile_map.h"
+
+//Files
+#include "file_system/file.h"
+//#include "file_system/mesh_loader/wavefront_loader.h"
+
+//Debug
+#include "debug/debug.h"
+#include "debug/performance.h"
+
+//Audio
+#include "audio/audio.h"
+#include "audio/audio_clip.h"
+#include "audio/audio_source.h"
