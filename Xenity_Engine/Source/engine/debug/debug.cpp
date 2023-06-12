6a1d65d7868f7c293b8397778e14d4088ec211fe
Fewnity <electronic.microprosse@gmail.com>
2023-06-12 16:02:02 +0200

    Add Error and Warning debug print

--- Xenity_Engine/Source/engine/debug/debug.cpp
+++ Xenity_Engine/Source/engine/debug/debug.cpp
@@ -13,22 +13,41 @@ File *file = nullptr;
 std::ofstream Debug::debugFile;
 
 /**
- * @brief Write text in the debug file
+ * Print an error in the console and the debug file
+ */
+void Debug::PrintError(std::string text)
+{
+    std::string newString = "[ERROR] " + text;
+    Print(newString);
+}
+
+/**
+ * Print a warning in the console and the debug file
+ */
+void Debug::PrintWarning(std::string text)
+{
+    std::string newString = "[WARNING] " + text;
+    Print(newString);
+}
+
+/**
+ * @brief Print text in the console and the debug file
  */
 void Debug::Print(std::string text)
 {
     text += '\n';
 #ifdef __PSP__
     // PspDebugPrint(text);
-    file->Write(text);
+    if (file)
+        file->Write(text);
 #elif __vita__
     // PsVitaDebugPrint(text);
     debugFile << text;
     debugFile.flush();
 #else
+    std::cout << text;
     debugFile << text;
     debugFile.flush();
-    std::cout << text;
 #endif
 }
 
@@ -40,16 +59,19 @@ void Debug::Init()
 {
 #ifdef __PSP__
     // PspDebugInit();
+    // Delete old debug file
     FileSystem::fileSystem->DeleteFile("xenity_engine_debug.txt");
+    // Init debug file
     file = new File("xenity_engine_debug.txt");
 #elif __vita__
     // PsVitaDebugInit();
+    // Create folder
     sceIoMkdir("ux0:/data/xenity_engine", 0777);
+    // Init debug file
     debugFile.open("ux0:data/xenity_engine/xenity_engine_debug.txt");
 #else
+    // Init debug file
     debugFile.open("xenity_engine_debug.txt");
 #endif
-    // Init debug file
-    // debugFile.open("xenity_engine_debug.txt");
     Print("-------- Debug initiated --------");
 }
--- Xenity_Engine/Source/engine/debug/debug.h
+++ Xenity_Engine/Source/engine/debug/debug.h
@@ -10,6 +10,8 @@ public:
     static void Init();
     static std::ofstream debugFile;
     static void Print(std::string text);
+    static void PrintError(std::string text);
+    static void PrintWarning(std::string text);
 
 private:
 };
