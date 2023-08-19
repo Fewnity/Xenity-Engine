f79a5e0c14c0efe5b098ad9f8b8e817d55594b4c
Fewnity <electronic.microprosse@gmail.com>
2023-08-20 01:51:19 +0200

    Add comments to Noise class

--- Xenity_Engine/Source/engine/noise/noise.h
+++ Xenity_Engine/Source/engine/noise/noise.h
@@ -9,19 +9,37 @@
 #pragma once
 
 #include <time.h>
+
 class API Noise
 {
 public:
-static float grad1(int hash, float x);
-static float grad2(int hash, float x, float y);
-static float grad3(int hash, float x, float y, float z);
-static float grad4(int hash, float x, float y, float z, float t);
-static float noise1(float x);
-static float pnoise1(float x, int px);
-static float noise2(float x, float y);
-static float pnoise2(float x, float y, int px, int py);
-static float noise3(float x, float y, float z);
-static float pnoise3(float x, float y, float z, int px, int py, int pz);
-static float noise4(float x, float y, float z, float w);
-static float pnoise4(float x, float y, float z, float w, int px, int py, int pz, int pw);
+
+	/**
+	* Get 1D noise
+	*/
+	static float noise1(float x);
+
+	/**
+	* Get 2D noise
+	*/
+	static float noise2(float x, float y);
+
+	/**
+	* Get 3D noise
+	*/
+	static float noise3(float x, float y, float z);
+
+	/**
+	* Get 4D noise
+	*/
+	static float noise4(float x, float y, float z, float w);
+private:
+	static float grad1(int hash, float x);
+	static float grad2(int hash, float x, float y);
+	static float grad3(int hash, float x, float y, float z);
+	static float grad4(int hash, float x, float y, float z, float t);
+	static float pnoise1(float x, int px);
+	static float pnoise2(float x, float y, int px, int py);
+	static float pnoise3(float x, float y, float z, int px, int py, int pz);
+	static float pnoise4(float x, float y, float z, float w, int px, int py, int pz, int pw);
 };
