1d0ee0d6439bb889222d32b1d85fbd68157c9db1
Fewnity <electronic.microprosse@gmail.com>
2023-08-19 23:49:38 +0200

    Add comments to Time class

--- Xenity_Engine/Source/engine/time/time.h
+++ Xenity_Engine/Source/engine/time/time.h
@@ -13,14 +13,47 @@
 class API Time
 {
 public:
-	static float GetTime();
-	static float GetUnscaledTime();
-	static float GetDeltaTime();
-	static float GetUnscaledDeltaTime();
-	static float GetTimeScale();
-	static void SetTimeScale(float timeScale);
-	static void UpdateTime();
+
+	/**
+	* [Internal] Init time system
+	*/
 	static void Init();
+
+	/**
+	* [Internal] Update time values
+	*/
+	static void UpdateTime();
+
+	/**
+	* Get total scaled elapsed time
+	*/
+	static float GetTime();
+
+	/**
+	* Get total unscaled elapsed time
+	*/
+	static float GetUnscaledTime();
+
+	/**
+	* Get scaled delta time
+	*/
+	static float GetDeltaTime();
+
+	/**
+	* Get unscaled delta time
+	*/
+	static float GetUnscaledDeltaTime();
+
+	/**
+	* Get time scale
+	*/
+	static float GetTimeScale();
+
+	/**
+	* Set time scale
+	* @param timeScale Time scale (minium 0)
+	*/
+	static void SetTimeScale(float timeScale);
 
 private:
 	static float timeScale;
