b35d853fa3b8fd952b584f08ef994489cd99bd04
Fewnity <electronic.microprosse@gmail.com>
2023-08-21 03:25:36 +0200

    Cancel compilation if no folder is selected

--- Xenity_Engine/Source/editor/ui/menus/main_bar_menu.cpp
+++ Xenity_Engine/Source/editor/ui/menus/main_bar_menu.cpp
@@ -46,27 +46,32 @@ void MainBarMenu::Draw()
 		if (ImGui::MenuItem("Build for Windows"))
 		{
 			std::string exportPath = EditorUI::OpenFolderDialog("Select an export folder");
-			Compiler::CompileGameThreaded(Platform::P_Windows, BuildType::BuildGame, exportPath);
+			if (exportPath != "")
+				Compiler::CompileGameThreaded(Platform::P_Windows, BuildType::BuildGame, exportPath);
 		}
 		if (ImGui::MenuItem("Build for PSP"))
 		{
 			std::string exportPath = EditorUI::OpenFolderDialog("Select an export folder");
-			Compiler::CompileGameThreaded(Platform::P_PSP, BuildType::BuildGame, exportPath);
+			if (exportPath != "")
+				Compiler::CompileGameThreaded(Platform::P_PSP, BuildType::BuildGame, exportPath);
 		}
 		if (ImGui::MenuItem("Build for PsVita"))
 		{
 			std::string exportPath = EditorUI::OpenFolderDialog("Select an export folder");
-			Compiler::CompileGameThreaded(Platform::P_PsVita, BuildType::BuildGame, exportPath);
+			if (exportPath != "")
+				Compiler::CompileGameThreaded(Platform::P_PsVita, BuildType::BuildGame, exportPath);
 		}
 		if (ImGui::MenuItem("Build And Run on Windows"))
 		{
 			std::string exportPath = EditorUI::OpenFolderDialog("Select an export folder");
-			Compiler::CompileGameThreaded(Platform::P_Windows, BuildType::BuildAndRunGame, exportPath);
+			if (exportPath != "")
+				Compiler::CompileGameThreaded(Platform::P_Windows, BuildType::BuildAndRunGame, exportPath);
 		}
 		if (ImGui::MenuItem("Build And Run on PSP"))
 		{
 			std::string exportPath = EditorUI::OpenFolderDialog("Select an export folder");
-			Compiler::CompileGameThreaded(Platform::P_PSP, BuildType::BuildAndRunGame, exportPath);
+			if(exportPath != "")
+				Compiler::CompileGameThreaded(Platform::P_PSP, BuildType::BuildAndRunGame, exportPath);
 		}
 		if (ImGui::MenuItem("Exit"))
 		{
