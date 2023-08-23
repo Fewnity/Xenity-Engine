c73b103d66b5d81ef6d214bdcf664b8498c63843
Fewnity <electronic.microprosse@gmail.com>
2023-08-23 19:59:30 +0200

    Unselect selected gameObject or file when clicking
    at an empty area of the hierarchy

--- Xenity_Engine/Source/editor/ui/menus/hierarchy_menu.cpp
+++ Xenity_Engine/Source/editor/ui/menus/hierarchy_menu.cpp
@@ -12,16 +12,24 @@ void HierarchyMenu::Draw()
 	ImGui::Begin("Hierarchy", 0, ImGuiWindowFlags_NoCollapse);
 
 	ImGui::BeginChild("Hierarchy list", ImVec2(0, 0), true);
+	bool itemClicked = false;
 	//Add in the list only gameobject without parent
 	for (int i = 0; i < Engine::gameObjectCount; i++)
 	{
 		if (Engine::gameObjects[i]->parent.lock() == nullptr)
 		{
-			EditorUI::DrawTreeItem(Engine::gameObjects[i]);
+			if (EditorUI::DrawTreeItem(Engine::gameObjects[i])) 
+			{
+				itemClicked = true;
+			}
 		}
 	}
-
 	isFocused = ImGui::IsWindowFocused();
+	if (ImGui::IsMouseClicked(0) && ImGui::IsWindowHovered())
+	{
+		Engine::selectedGameObject.reset();
+		Engine::selectedFileReference = nullptr;
+	}
 	ImGui::EndChild();
 
 	ImGui::End();
