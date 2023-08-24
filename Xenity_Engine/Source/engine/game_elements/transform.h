f4c658c3ff1454acca6374da3fe71f0400c4d89c
Fewnity <electronic.microprosse@gmail.com>
2023-08-24 18:45:14 +0200

    Add comments to Transform class

--- Xenity_Engine/Source/engine/game_elements/transform.h
+++ Xenity_Engine/Source/engine/game_elements/transform.h
@@ -27,43 +27,143 @@ public:
 
 	virtual ~Transform() = default;
 
+	/**
+	* Get position
+	*/
 	Vector3 GetPosition() const;
+
+	/**
+	* Get local position
+	*/
 	Vector3 GetLocalPosition() const;
-	Vector3 GetRotation() const;//Euler angle
-	Vector3 GetLocalRotation() const;//Euler angle
+
+	/**
+	* Get rotation (in degree)
+	*/
+	Vector3 GetRotation() const;
+
+	/**
+	* Get locla rotation (in degree)
+	*/
+	Vector3 GetLocalRotation() const;
+
+	/**
+	* Get scale
+	*/
 	Vector3 GetScale() const;
+
+	/**
+	* Get local scale
+	*/
 	Vector3 GetLocalScale() const;
 
+	/**
+	* Get forward direction
+	*/
 	Vector3 GetForward() const;
+
+	/**
+	* Get left direction
+	*/
 	Vector3 GetLeft() const;
+
+	/**
+	* Get right direction
+	*/
 	Vector3 GetRight() const;
+
+	/**
+	* Get up direction
+	*/
 	Vector3 GetUp() const;
+
+	/**
+	* Get down direction
+	*/
 	Vector3 GetDown() const;
+
+	/**
+	* Get backward direction
+	*/
 	Vector3 GetBackward() const;
 
-
+	/**
+	* Set position
+	* @param value Position
+	*/
 	void SetPosition(const Vector3 value);
+
+	/**
+	* Set local position
+	* @param value Local position
+	*/
 	void SetLocalPosition(const Vector3 value);
+
+	/**
+	* Set rotation (in degree)
+	* @param value Rotation (in degree)
+	*/
 	void SetRotation(const Vector3 value);//Euler angle
+
+	/**
+	* Set local rotation (in degree)
+	* @param value Local rotation (in degree)
+	*/
 	void SetLocalRotation(const Vector3 value);//Euler angle
+
+	/**
+	* Set local scale
+	* @param value Local scale
+	*/
 	void SetLocalScale(const Vector3 value);
+
+	/**
+	* [Internal] Update children world positions
+	*/
 	void SetChildrenWorldPositions();
+
+	/**
+	* Function called when the parent changed
+	*/
 	void OnParentChanged();
+
 	glm::mat4 transformationMatrix;
 	bool movedLastFrame = true;
 
+	/**
+	* Get GameObject
+	*/
 	std::shared_ptr<GameObject> GetGameObject()
 	{
 		return gameObject.lock();
 	}
 
+	/**
+	* [Internal] Update world values
+	*/
 	void UpdateWorldValues();
 	bool isTransformationMatrixDirty = true;
 
 private:
+
+	/**
+	* Update transformation matrix
+	*/
 	void UpdateTransformationMatrix();
+
+	/**
+	* Update world position
+	*/
 	void UpdateWorldPosition();
+
+	/**
+	* Update world rotation
+	*/
 	void UpdateWorldRotation();
+
+	/**
+	* Update world scale
+	*/
 	void UpdateWorldScale();
 
 	Vector3 position = Vector3(0);
@@ -74,7 +174,15 @@ private:
 	Vector3 localScale = Vector3(1);
 	std::weak_ptr<GameObject> gameObject;
 	float rotationMatrix[9] = { 0,0,0,0,0,0,0,0,0 };
+
+	/**
+	* Get localPosition from matrices 
+	*/
 	Vector3 GetLocalPositionFromMatrices(glm::mat4 childMatrix, glm::mat4 parentMatrix);
+
+	/**
+	*
+	*/
 	Vector3 GetLocalRotationFromWorldRotations(Vector3 childWorldRotation, Vector3 parentWorldRotation);
 
 };
