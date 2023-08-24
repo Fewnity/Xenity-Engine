30ffba47db7d84f676ff6a6b8adfb60f84d9091c
Fewnity <electronic.microprosse@gmail.com>
2023-08-24 18:44:44 +0200

    Add comments to Unique Id class

--- Xenity_Engine/Source/engine/unique_id/unique_id.h
+++ Xenity_Engine/Source/engine/unique_id/unique_id.h
@@ -7,11 +7,17 @@ public:
 	UniqueId();
 	UniqueId(bool forFile);
 
+	/**
+	* Get unique Id
+	*/
 	uint64_t GetUniqueId()
 	{
 		return uniqueId;
 	}
 
+	/**
+	* Set unique Id
+	*/
 	void SetUniqueId(uint64_t id)
 	{
 		uniqueId = id;
@@ -19,6 +25,12 @@ public:
 
 	static uint64_t lastFileUniqueId;
 	static uint64_t lastUniqueId;
+
+	/**
+	* Generate a new id
+	* @param forFile Is an Id for a file
+	* @return new Id
+	*/
 	static uint64_t GenerateUniqueId(bool forFile);
 private:
 	bool forFile = false;
