dc4dc4c8a7deb3bd65231d73dda5c2d397e86d69
Fewnity <electronic.microprosse@gmail.com>
2023-08-24 18:45:05 +0200

    Add comments to Sprite Manager class

--- Xenity_Engine/Source/engine/graphics/2d_graphics/sprite_manager.h
+++ Xenity_Engine/Source/engine/graphics/2d_graphics/sprite_manager.h
@@ -18,9 +18,28 @@ class Transform;
 class API SpriteManager
 {
 public:
-    static void DrawSprite(Vector3 position, Vector3 rotation, Vector3 scale, Texture *texture, Color color, std::weak_ptr<Transform> transform);
-    static void Render2DLine(MeshData *meshData);
+    
+    /**
+    * Init sprite manager
+    */
     static void Init();
+
+    /**
+    * Draw a sprite
+    * @param position Sprite's position
+    * @param rotation Sprite's roation
+    * @param scale Sprite's scale
+    * @param texture Sprite's texture
+    * @param color Sprite's color
+    * @param transform Sprite's transform
+    */
+    static void DrawSprite(Vector3 position, Vector3 rotation, Vector3 scale, Texture *texture, Color color, std::weak_ptr<Transform> transform);
+    
+    /**
+    * Render a 2D line
+    * @param meshData Mesh data
+    */
+    static void Render2DLine(MeshData *meshData);
 
 private:
     static MeshData *spriteMeshData;
