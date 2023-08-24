bf514fff6974b63e59caedd41033b39b6249b77c
Fewnity <electronic.microprosse@gmail.com>
2023-08-24 18:46:19 +0200

    Add comments to Component class

--- Xenity_Engine/Source/engine/component.h
+++ Xenity_Engine/Source/engine/component.h
@@ -22,30 +22,62 @@ public:
 	Component();
 
 	virtual ~Component();
+	
+	/**
+	* Function called once before Start() at the creation of the component
+	*/
 	virtual void Awake() {}
+	
+	/**
+	* Function called once after Awake() at the creation of the component
+	*/
 	virtual void Start() {}
+
+	/**
+	* Function called every frame
+	*/
 	virtual void Update() {}
 	//virtual void OnParentChanged() {}
 
 	int updatePriority = 5000; //Lower is more priotary
+	
+	/**
+	* Get if the component is enabled
+	*/
 	bool GetIsEnabled();
+	
+	/**
+	* Enable or disable the component
+	*/
 	void SetIsEnabled(bool isEnabled);
 	bool initiated = false;
 
 	bool waitingForDestroy = false;
 
+	/**
+	* Set component's GameObject
+	*/
 	void SetGameObject(std::weak_ptr<GameObject> go);
 
+	/**
+	* Get component's GameObject
+	*/
 	std::shared_ptr <GameObject> GetGameObject() const
 	{
 		return gameObject.lock();
 	}
 
+	/**
+	* Get component's Transform
+	*/
 	std::shared_ptr <Transform> GetTransform() const
 	{
 		return transform.lock();
 	}
 
+	/**
+	* Get component's name
+	*/
 	std::string GetComponentName() 
 	{
 		return componentName;
