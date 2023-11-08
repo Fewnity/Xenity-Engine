25455ca98e55f143f380bd372a3f42dab536d6e8
Fewnity <electronic.microprosse@gmail.com>
2023-11-08 12:17:44 +0100

    Set process's priority to high

--- Xenity_Engine/Source/windows/cpu.cpp
+++ Xenity_Engine/Source/windows/cpu.cpp
@@ -1,9 +1,11 @@
 #if defined(_WIN32) || defined(_WIN64)
 
 #include "../engine/cpu.h"
+#include <windows.h>
 
 void SetMaxCpuSpeed()
 {
+	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
 }
 
 #endif
