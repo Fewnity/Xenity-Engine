07bcea06cda13a1f725c26ced2ae34e0ec58fcde
Fewnity <electronic.microprosse@gmail.com>
2024-11-14 18:32:51 +0100

    Fix PSP build

--- Xenity_Engine/Source/engine/graphics/shader_null.h
+++ Xenity_Engine/Source/engine/graphics/shader_null.h
@@ -14,4 +14,39 @@
 class ShaderNull : public Shader
 {
 public:
+	void Load() override {}
+	void CreateShader(Shader::ShaderType type) override {}
+
+	bool Use() override { return false; }
+
+	void SetShaderCameraPosition() override {}
+
+	void SetShaderCameraPositionCanvas() override {}
+
+	void SetShaderProjection() override {}
+
+	void SetShaderProjectionCanvas() override {}
+
+	void SetShaderModel(const glm::mat4& trans) override {}
+
+	void SetShaderModel(const Vector3& position, const Vector3& eulerAngle, const Vector3& scale) override {}
+
+	void SetLightIndices(const LightsIndices& lightsIndices) override {}
+
+	void SetShaderAttribut(const std::string& attribut, const Vector4& value) override {}
+	void SetShaderAttribut(const std::string& attribut, const Vector3& value) override {}
+	void SetShaderAttribut(const std::string& attribut, const Vector2& value) override {}
+	void SetShaderAttribut(const std::string& attribut, float value) override {}
+	void SetShaderAttribut(const std::string& attribut, int value) override {}
+
+	void UpdateLights(bool disableLights) override {}
+
+	void Link() override {}
+
+	bool Compile(const std::string& filePath, ShaderType type) override { return false; }
+
+	void SetPointLightData(const Light& light, const int index) override {}
+	void SetDirectionalLightData(const Light& light, const int index) override {}
+	void SetAmbientLightData(const Vector3& color) override {}
+	void SetSpotLightData(const Light& light, const int index) override {}
 };
