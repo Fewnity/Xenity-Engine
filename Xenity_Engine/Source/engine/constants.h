59f13b8d667c278b632d79fb6808358cf215b096
Fewnity <electronic.microprosse@gmail.com>
2025-01-01 15:03:30 +0100

    Disable ENABLE_SHADER_VARIANT_OPTIMIZATION

--- Xenity_Engine/Source/engine/constants.h
+++ Xenity_Engine/Source/engine/constants.h
@@ -5,7 +5,7 @@
 //
 //#define ENABLE_EXPERIMENTAL_FEATURES // Enable features that are not fully tested or implemented
 //#define ENABLE_OVERDRAW_OPTIMIZATION // Enable overdraw optimization (currently not great)
-#define ENABLE_SHADER_VARIANT_OPTIMIZATION // Enable shader variant optimization (currently effective only on PS3, WIP)
+//#define ENABLE_SHADER_VARIANT_OPTIMIZATION // Enable shader variant optimization (currently effective only on PS3, WIP)
 
 #if defined(__PS3__)
 //#define ENABLE_OVERDRAW_OPTIMIZATION
