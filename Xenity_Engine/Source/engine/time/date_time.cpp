84e192f07d49948351f6552929823892bf0c3982
Fewnity <electronic.microprosse@gmail.com>
2025-07-25 00:13:00 +0200

    Add missing license header

--- Xenity_Engine/Source/engine/time/date_time.cpp
+++ Xenity_Engine/Source/engine/time/date_time.cpp
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2022-2025 Gregory Machefer (Fewnity)
+//
+// This file is part of Xenity Engine
+
 #include "date_time.h"
 
 #if defined(__PSP__)
